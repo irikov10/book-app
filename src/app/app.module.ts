645af825ea7184739c787f28383573aa393677e2
irikov10 <evgenijirikov10@gmail.com>
2023-07-19 20:17:25 +0300

    Fixed stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { HomeComponent } from './home/home.component';
 import { UserModule } from './user/user.module';
 import { BookCollectionComponent } from './book-collection/book-collection.component';
 import { FavoriteBooksComponent } from './favorite-books/favorite-books.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     BookCollectionComponent,
-    FavoriteBooksComponent
+    FavoriteBooksComponent,
+    BookDetailsComponent
   ],
   imports: [
     BrowserModule,
